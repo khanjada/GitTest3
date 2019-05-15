5c4a0bc577d3ba3d8151305beaab7664a5f8b5a0
hurayra <khanjada.hurayra.qa@gmail.com>
2019-05-14 22:44:36 -0400

    commit for Float

---
+++ src/javaclass/FloatDataType.java
@@ -0,0 +1,15 @@
+package javaclass;
+
+public class FloatDataType {
+
+
+        public static void main (String[]args){
+
+            Float number= 29.20f;
+
+
+            System.out.println(number);
+        }
+
+
+}
